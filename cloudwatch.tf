071177608c352c0a9705953cf5d0e1aa326ab56c
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-08-10 16:26:26 +0100

    Add throughput CW alarm

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,11 +1,14 @@
 locals {
     warning_capacity_threshold = var.storage_capacity * 1000000000 * 0.15
     critical_capacity_threshold = var.storage_capacity * 1000000000 * 0.10
+
+    
+    critical_throughput_threshold = var.throughput_capacity * 1000000000 * 0.90
     fsx_name = lookup(var.tags, "Name", "Unkown name")
 }
 
 resource "aws_cloudwatch_metric_alarm" "free_space_warning" {
-  alarm_name          = "free_space_warning_15_percent - ${local.fsx_name}"
+  alarm_name          = "${local.fsx_name} - free_space_warning_15_percent"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "3"
   metric_name         = "FreeStorageCapacity"
@@ -26,7 +29,7 @@ resource "aws_cloudwatch_metric_alarm" "free_space_warning" {
 
 
 resource "aws_cloudwatch_metric_alarm" "free_space_critical" {
-  alarm_name          = "free_space_critical_10_percent - ${local.fsx_name}"
+  alarm_name          = "${local.fsx_name} - free_space_critical_10_percent"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "3"
   metric_name         = "FreeStorageCapacity"
@@ -40,6 +43,63 @@ resource "aws_cloudwatch_metric_alarm" "free_space_critical" {
   }
 
   alarm_description = "CRITICAL - Less than 10% Free storage available on FSx filesystem"
+  alarm_actions     = [var.sns_critical]
+  ok_actions        = [var.sns_warning]
+  insufficient_data_actions = [var.sns_critical]
+}
+
+
+
+resource "aws_cloudwatch_metric_alarm" "throughput_usage_critical" {
+  alarm_name          = "${local.fsx_name} - throughput_usage_critical"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "3"
+  metric_name         = "TotalThroughputUsage"
+  namespace           = "AWS/FSx"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = local.critical_capacity_threshold
+
+metric_query {
+    id          = "e1"
+    expression  = "m2+m1"
+    label       = "Total Throughput"
+    return_data = "true"
+  }
+
+  metric_query {
+    id = "m1"
+
+    metric {
+      metric_name = "DataWriteBytes"
+      namespace   = "AWS/FSx"
+      period      = "120"
+      stat        = "Sum"
+      unit        = "Average"
+
+      dimensions = {
+        FileSystemId = element(concat(aws_fsx_windows_file_system.default.*.id, list("")),0)
+      }
+    }
+  }
+
+ metric_query {
+    id = "m2"
+
+    metric {
+      metric_name = "DataReadBytes"
+      namespace   = "AWS/FSx"
+      period      = "120"
+      stat        = "Sum"
+      unit        = "Average"
+
+      dimensions = {
+        FileSystemId = element(concat(aws_fsx_windows_file_system.default.*.id, list("")),0)
+      }
+    }
+  }
+
+  alarm_description = "CRITICAL - Throughput Usage over 90% on FSx filesystem"
   alarm_actions     = [var.sns_critical]
   ok_actions        = [var.sns_warning]
   insufficient_data_actions = [var.sns_critical]

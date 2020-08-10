8c05cd0be2b1169854929bf5c1a0f8f4f05c925a
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-08-10 10:36:31 +0100

    Update outputs with count index

--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
 output "id" {
   description = "Identifier of the file system"
-  value       = aws_fsx_windows_file_system.default.id
+  value       = aws_fsx_windows_file_system.default.id[count.index]
 }
 
 output "arn" {
   description = "Amazon Resource Name of the file system."
-  value       = aws_fsx_windows_file_system.default.arn
+  value       = aws_fsx_windows_file_system.default.arn[count.index]
 }
 
 output "dns_name" {
   description = "DNS name for the file system, e.g. fs-12345678.corp.example.com (domain name matching the Active Directory domain name)"
-  value       = aws_fsx_windows_file_system.default.dns_name
+  value       = aws_fsx_windows_file_system.default.dns_name[count.index]
 }
 
 output "preferred_file_server_ip" {
   description = "The IP address of the primary, or preferred, file server."
-  value       = aws_fsx_windows_file_system.default.preferred_file_server_ip
+  value       = aws_fsx_windows_file_system.default.preferred_file_server_ip[count.index]
 }
 
 output "remote_administration_endpoint" {
   description = "For MULTI_AZ_1 deployment types, use this endpoint when performing administrative tasks on the file system using Amazon FSx Remote PowerShell. For SINGLE_AZ_1 deployment types, this is the DNS name of the file system."
-  value       = aws_fsx_windows_file_system.default.remote_administration_endpoint
+  value       = aws_fsx_windows_file_system.default.remote_administration_endpoint[count.index]
 }
